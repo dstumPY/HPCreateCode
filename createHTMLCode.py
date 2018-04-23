fb3258eff222cdcef6dab0ea219871ce383c49c7
dstum01 <35544135+dstum01@users.noreply.github.com>
2018-04-23 18:44:21 +0200

    Update createHTMLCode.py

--- createHTMLCode.py
+++ createHTMLCode.py
@@ -124,7 +124,7 @@ def writeOutput(dic):
 	translateDays = {"Monday" : "Montag", "Tuesday" : "Dienstag" , "Wednesday" : "Mittwoch" , "Thursday" : "Donnerstag" , "Friday" : "Freitag" , "Saturday" : "Samstag" , "Sunday" : "Sonntag"}
 	translateMonths = {"January" : "Januar", "February" : "Februar", "March" : "März", "April" : "April", "May" : "Mai", "June" : "Juni", "July" : "Juli", "August" : "August", "September" : "September", "October" : "Oktober", "November" : "November" , "December" : "Dezember"}
 	timeFormat = {"0" : "00" , "1" : "01", "2" : "02", "3" : "03" ,"4" : "04", "5" : "05", "6" : "06", "7" : "07", "8" : "08", "9" : "09","10" : "10", "11" : "11", "12" : "12", "13" : "13", "14" : "14", "15" : "15", "16" : "16", "17" : "17", "18" : "18", "19" : "19", "20" : "20", "21" : "21", "22" : "22", "23" : "23", "24" : "24", "25" : "25", "26" : "26", "27" : "27", "28" : "28", "29" : "29", "30" : "30", "31" : "31", "32" : "32", "33" : "33", "34" : "34", "35" : "35", "36" : "36", "37" : "37", "38" : "38", "39" : "39", "40" : "40", "41" : "41", "42" : "42", "43" : "43", "44" : "44", "45" : "45", "46" : "46", "47" : "47", "48" : "48", "49" : "49", "50" : "50", "51" : "51", "52" : "52", "53" : "53", "54" : "54", "55" : "55", "56" : "56", "57" : "57", "58" : "58", "59" : "59"}
-	tmpString = "Schovelkoten_HP_EVENTCODE_" + str(currentDate.tm_mday) + "-" + str(currentDate.tm_mon) + "-" + str(currentDate.tm_year) + ".txt"
+	tmpString = "HP_EVENTCODE_" + str(currentDate.tm_mday) + "-" + str(currentDate.tm_mon) + "-" + str(currentDate.tm_year) + ".txt"
 	fobj = open(tmpString, "w")
 	#fobj.write("Hallo")
 	for dicElement in sorted(dic.keys()):
